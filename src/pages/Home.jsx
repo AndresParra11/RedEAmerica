eb7c5c11144a47b78e9585fea9eff4bcc0a6c323
Andrés Parra <pipe.parrab@gmail.com>
2023-11-24 20:32:41 -0500

    Listo redirección a login y registro de home.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+    </div>
+  );
 };
 
 export default Home;
